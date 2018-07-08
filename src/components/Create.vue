d65723e1bfb1782c31c90f5b849c53d9440f8f86
Susie <susannahirene@gmail.com>
2018-07-08 17:18:41 -0500

    options select workflow

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,33 +1,109 @@
 <template>
   <div class="create">
       <div class="create-container">
-      <h2>Select Jewelry Type</h2>
       
-        <div class="select-type">
-          <div class="column">
-              <p>necklace</p>
+      
+        <div class="select-type-container" v-if="typeChosen === false">
+            <h2>Select Jewelry Type</h2>
+            <div class="select-type">
+          <div class="column" id="necklace" @click="selectNecklace">
+              <p class="select-text">necklace</p>
             </div>
           
-            <div class="column">
-                <p>bracelet</p>
+            <div class="column" id="bracelet" @click="selectBracelet">
+                <p class="select-text">bracelet</p>
             </div>
-          
+            </div>
           
           </div>
-      
+          
+          <div class="select-length" v-if="typeChosen === true">
+              <h2>Select Length</h2>
+          <div v-if="necklace === true">
+              <p>{{ selectedType }} lengths:</p>
+              
+              <input type="radio" id="small" value="12 in" v-model="necklaceLength"/>
+              <label for="small">12"</label>
+              
+              <input type="radio" id="medium" value="14 in" v-model="necklaceLength"/>
+              <label for="medium">14"</label>
+              
+              <input type="radio" id="large" value="16 in" v-model="necklaceLength"/>
+              <label for="large">16"</label>
+              
+              <p>Length: {{ necklaceLength }}</p>
+          
+          </div>
+          
+          <div v-if="bracelet === true">
+              <p>{{ selectedType }} lengths:</p>
+                
+            <input type="radio" id="s" value="6 inches" @change="showButton(braceletLength)" v-model="braceletLength"/>
+            <label for="s">6"</label>
+              
+            <input type="radio" id="m" value="7 inches" @change="showButton(braceletLength)" v-model="braceletLength"/>
+            <label for="m">7"</label>
+              
+            <input type="radio" id="l" value="8 inches" @change="showButton(braceletLength)" v-model="braceletLength"/>
+            <label for="l">8"</label>
+              
+            <input type="radio" id="xl" value="9 inches" @change="showButton(braceletLength)" v-model="braceletLength"/>
+            <label for="xl">9"</label>
+              
+            <p>Length: {{ braceletLength }}</p>
+          
+          </div>
+              
+          <button class="btn" v-if="lengthChosen === true">select your beads</button>
+              
+          </div>
+          
+          
       
       </div>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'Create',
   data () {
     return {
-
+        necklace: false,
+        bracelet: false,
+        typeChosen: false,
+        lengthChosen: false,
+        selectedType: '',
+        necklaceLength: '',
+        braceletLength: ''
+        
     }
-  }
+  },
+    
+    
+    methods: {
+        
+        showButton: function(braceletLength){
+            this.lengthChosen = true;
+            this.braceletLength = braceletLength;
+        },
+        
+        selectNecklace: function(){
+            this.necklace = true;
+            this.bracelet = false;
+            this.typeChosen = true;
+            this.selectedType = 'Necklace';
+            
+        },
+        
+        selectBracelet: function(){
+            this.bracelet = true;
+            this.necklace = false;
+            this.typeChosen = true;
+            this.selectedType = 'Bracelet';
+        }
+    }
 }
 </script>
 
@@ -49,11 +125,15 @@ export default {
     padding-top: 10px;
     padding-bottom: 10px;
     min-height: 700px;
-    border: 1px solid #ddd;
+    
     }
     
     .create-container h2 {
     text-align: center;
+    }
+    
+    .select-type-container {
+        
     }
     
     .select-type {
@@ -61,15 +141,34 @@ export default {
         justify-content: center;
     grid-template-columns: auto auto;
     max-width: 700px;
-    grid-gap: 10px;
-    border: 1px solid #ddd;
+    grid-gap: 20px;
+    padding: 10px;
     }
     
     .column {
-    width: 350px;
-    height: 350px;
-    background-color: #f4f4f4;
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    width: 200px;
+    height: 200px;
+    background-color: #F4F4F4;
+    transition: transform .3s;
+    cursor: pointer;
+    }
+    
+    .column:hover {
+    transform: scale(1.05);
+    }
+    
+    .select-text {
+    font-size: 22px;
+line-height: 25px;
+color: #262626;
+font-family: 'Playfair Display';
+font-weight: 400;
     text-align: center;
+    margin: auto;
+    padding: 0;
     }
 
 </style>
---
+++ src/components/LengthSelector.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="length-selector">
+<h2>Select Length</h2>
+      {{ type }}
+      <div v-if="necklace === true">
+      
+      <p>You chose necklace.</p>
+      </div>
+      
+      <div v-if="bracelet === true">
+          <p>You chose bracelet.</p>
+      
+      </div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LengthSelector',
+    props: ['type'],
+  data () {
+    return {
+        selectedLength: ''
+
+    }
+  },
+    
+    computed: {
+        necklace(){
+            this.type = 'necklace';
+        },
+        
+        bracelet(){
+            this.type = 'bracelet';
+        }
+    }
+}
+</script>
+
+<style>
+    
+.length-selector {
+    display: grid;
+    align-content: flex-start;
+        justify-content: center;
+    grid-gap: 30px;
+   margin-left: 100px;
+    margin-right: 100px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    min-height: 700px;
+    
+    }
+    
+.length-selector h2 {
+    text-align: center;
+    }
+
+</style>
