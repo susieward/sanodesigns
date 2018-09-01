d7a60d2ac8a633dfebee13c33d9acb3333408202
Susie <susannahirene@gmail.com>
2018-09-01 02:18:36 -0500

    sdfsfs

--- src/components/NecklaceSelector.vue
+++ src/components/NecklaceSelector.vue
@@ -27,7 +27,7 @@
       <div class="necklace-selector-container">
 
           <div class="design-necklace" v-if="selectedBeads.length && editingBeads === false">
-     <div class="necklace-template">
+     <div class="necklace-template" style="border: 1px solid red">
               
               <div class="necklace-template-content">
               
